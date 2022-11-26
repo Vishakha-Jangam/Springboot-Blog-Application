34ea3db5241cb507e7d4b191ed4c6426d995ff91
Vishakha-Jangam <101566433+Vishakha-Jangam@users.noreply.github.com>
2022-11-26 23:08:34 +0530

    Basic Application Structure

---
+++ src/test/java/com/masai/springboot_blogApp/SpringbootBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai.springboot_blogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
