c37be7e24f50aae9013e32464262c59d3e9f23b9
Vishakha-Jangam <101566433+Vishakha-Jangam@users.noreply.github.com>
2022-12-07 22:47:51 +0530

    default Spring Security added

---
+++ src/main/java/com/masai/springboot_blogApp/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.masai.springboot_blogApp.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+	
+	
+}
