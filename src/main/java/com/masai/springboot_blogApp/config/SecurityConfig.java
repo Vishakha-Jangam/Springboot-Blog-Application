9f1bfa0bf84fdd966a8ebfd51219f51bc1972aff
Vishakha-Jangam <101566433+Vishakha-Jangam@users.noreply.github.com>
2022-12-08 22:14:13 +0530

    .

--- src/main/java/com/masai/springboot_blogApp/config/SecurityConfig.java
+++ src/main/java/com/masai/springboot_blogApp/config/SecurityConfig.java
@@ -2,12 +2,11 @@ package com.masai.springboot_blogApp.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig{
+	
 	
 	
 }
