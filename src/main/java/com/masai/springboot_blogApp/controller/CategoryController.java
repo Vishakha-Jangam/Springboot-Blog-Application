77f669c6f414cc3a04fc45c5239c2c7bfbae8d53
Vishakha-Jangam <101566433+Vishakha-Jangam@users.noreply.github.com>
2023-01-15 21:58:36 +0530

    >

--- src/main/java/com/masai/springboot_blogApp/controller/CategoryController.java
+++ src/main/java/com/masai/springboot_blogApp/controller/CategoryController.java
@@ -6,16 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.masai.springboot_blogApp.DTO.CategoryDTO;
-import com.masai.springboot_blogApp.entity.Category;
 import com.masai.springboot_blogApp.service.CategoryService;
 
 @RestController
